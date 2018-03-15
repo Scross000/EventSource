3fbe7b235e8e84e4f15920fa6baa2575ae4dba38
Kevin Hartarto <k.hartarto9@gmail.com>
2018-03-15 20:26:09 +0700

    main page

--- details/index.php
+++ details/index.php
@@ -1,15 +1,53 @@
+<?php
+	include('conn.php')
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Event Source</title>
 </head>
 <body>
 	<!-- header -->
 	<div>
-		
+		<nav>
+			
+		</nav>
 	</div>
 	<div>
-		
+		<!-- picture -->
+		<div>
+			<img src="lalala.jpg">
+		</div>
+
+		<!-- detail -->
+		<br>
+		<div>
+			<h3>Nama Event</h3>
+			<br>
+			<h3>periode event</h3>
+			<br>
+			<h3>Lokasi</h3>
+			<br>
+			<h3>Contact person</h3>
+		</div>
+		<!-- table harga -->
+		<div>
+			<table>
+				<thead>
+					<th>Jenis Tiket</th>
+					<th>Jumlah Tiket</th>
+					<th>Harga Tiket</th>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
+		</div>
+
+		<!-- detail lainnya -->
+		<div>
+			<p></p>
+		</div>
 	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,83 @@
 <?php 
 // include('connection.php');
-	include('conn.php');
+//	include('conn.php');
+	include('conn1.php');
 	session_start();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Event Source</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<?php
-		if(isset($_SESSION["user"]))
-		{
-			echo "<p>Hello, ".$_SESSION["user"]["username"]."</p>";
-			?>
-				<p><a href="login/logout.php">Logout</a></p>
-			<?php
-		}
-		else
-		{
-			?>
-				<p><a href="login">Login</a></p>
-			<?php
-		}
-	?>
-	<!-- tes -->
+	<!-- header -->
+	<div>
+		
+	</div>
+
+	<!-- Carousel -->
+	<div id="eventCarousel" class="carousel slide" data-ride="Carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#eventCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#eventCarousel" data-slide-to="1"></li>
+		    <li data-target="#eventCarousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+		    	<img src="la.jpg" alt="Los Angeles">
+			</div>
+
+			<div class="item">
+			    <img src="chicago.jpg" alt="Chicago">
+			</div>
+
+			<div class="item">
+			    <img src="ny.jpg" alt="New York">
+			</div>
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#eventCarousel" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		    <span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#eventCarousel" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		    <span class="sr-only">Next</span>
+  		</a>
+	</div>
+
+	<!-- Event lists -->
+	<div>
+		<!-- event table -->
+		<div style="margin-left: 220px;margin-top: 50px">
+			<table id="eventLists" border="bold" width="80%">
+				<thead>
+					<th>1</th>
+					<th>2</th>
+					<th>3</th>
+					<th>4</th>
+					<th>5</th>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>
+							<div align="right">
+								<p>judul</p>
+								<p>no</p>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
 </body>
 </html>
