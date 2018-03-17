abb05a08816cb146b796cbc3396a72caa38d21de
matemo22 <matemo2204@gmail.com>
2018-03-17 13:48:46 +0700

    (?)

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
 ?>
 <!DOCTYPE html>
 <html>
-<<<<<<< HEAD
 <head>
 	<title>Event Source</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
